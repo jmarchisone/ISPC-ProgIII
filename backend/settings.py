350d06ae1f46fe1eea7aa4d3cb84ac72e2b9881c
JMarchisone <jorgemarchisone@gmail.com>
2026-04-03 22:57:18 -0300

    Se carga postgresql en settings

--- backend/settings.py
+++ backend/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'backend_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Ispc.2024',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
