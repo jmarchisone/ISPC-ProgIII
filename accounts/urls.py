d580a31b36defcb0a7406059f13b795ea3588f94
JMarchisone <jorgemarchisone@gmail.com>
2026-04-05 11:05:08 -0300

    Implementacion del endpoint refresh proporcionado por SIMPLE_JWT

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import RegisterView, LoginView, PasswordResetRequestView, PasswordResetVerifyView, UserDetailView, ChangePasswordView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('password-reset-verify/', PasswordResetVerifyView.as_view(), name='password-reset-verify'),
     path('profile/', UserDetailView.as_view(), name='user-profile'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
